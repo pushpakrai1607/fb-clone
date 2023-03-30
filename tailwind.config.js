6009c9422fff594cbe9bfd892e7dd1a8b330f40c
pushpak rai <pushpakrai1607gmail.com>
2023-03-30 11:59:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13:"13px"
+      },
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '2' }],
+      }
+    },
+  },
+  plugins: [],
+}
